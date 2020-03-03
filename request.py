27be7ad24a6f8f5175cdfd8d163cbca218d4a396
Wad-man <44425782+Wad-man@users.noreply.github.com>
2020-03-03 09:48:58 +0300

    add 2 files
    2 files:
    request
    urls

---
+++ request.py
@@ -0,0 +1,15 @@
+import urls
+import requests
+import time
+
+f = open('request.csv', 'w')
+i = 1
+while i < 2: #1598:
+    url = urls.urls[i]
+    status = requests.get(url)
+    print(url, status.status_code)
+    f.write(str(i) + ';' + str(url) + ';' + str(status.status_code) + '\n')
+    time.sleep(1)
+    i = i + 1
+
+f.close()
